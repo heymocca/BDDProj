4153ba7c8a61831076c0639b530b7317720d7e78
Nikki <moccababy777@gmail.com>
2016-10-02 22:54:55 +1300

    create a new class

---
+++ src/cucmberTest/TestCucumber.java
@@ -0,0 +1,5 @@
+package cucmberTest;
+
+public class TestCucumber {
+
+}
---
+++ src/stepDefinition/Login_Test.java
@@ -0,0 +1,34 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Login_Test {
+	@Given("^User is on EmailTpreList Page$")
+	public void user_is_on_EmailTpreList_Page() throws Throwable {
+	}
+
+	@When("^User Navigate to Google Mail LogIn Page$")
+	public void user_Navigate_to_Google_Mail_LogIn_Page() throws Throwable {
+	}
+
+	@When("^User enters UserName and Password$")
+	public void user_enters_UserName_and_Password() throws Throwable {
+	}
+
+	@Then("^Message displayed Login Successfully$")
+	public void message_displayed_Login_Successfully() throws Throwable {
+	}
+
+	@When("^User LogOut from the Application$")
+	public void user_LogOut_from_the_Application() throws Throwable {
+	}
+
+	@Then("^Message displayed LogOut Successfully$")
+	public void message_displayed_LogOut_Successfully() throws Throwable {
+	}
+	
+	
+	
+}
