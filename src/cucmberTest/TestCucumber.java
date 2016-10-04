4aea31977901d4f9b2ffc09639bd8ed0e229ac4e
Nikki <moccababy777@gmail.com>
2016-10-04 21:48:05 +1300

    TestCucumber

--- src/cucmberTest/TestCucumber.java
+++ src/cucmberTest/TestCucumber.java
@@ -3,7 +3,14 @@ package cucmberTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestCucumber {
 
@@ -14,13 +21,39 @@ public class TestCucumber {
 	public void testLoginAndLogout() throws Exception {
 		driver.get(baseUrl);
 		driver.get("https://www.gmail.com/");
-		WebElement signIn = driver.findElement(By.xpath("//a[contains(text(),'Sign In')]"));
-		signIn.click();
-		WebElement userName = driver.findElement(By.xpath(".//*[@id='Email']"));
-		WebElement password = driver.findElement(By.xpath(".//*[@id='Passwd']"));
+		System.out.println("driver.get('https://www.gmail.com/');");	
+		
+		WebElement Email = driver.findElement(By.id("Email"));
+		Email.sendKeys("***@gmail.com");
+		System.out.println("Email");
+		WebElement next = driver.findElement(By.xpath(".//*[@id='next']"));
+		next.click();
+		
+		Thread.sleep(5000);
+		WebElement password = driver.findElement(By.id("Passwd"));
+		password.sendKeys("***");
+		System.out.println("Passwd");
 		WebElement loginButton = driver.findElement(By.xpath(".//*[@id='signIn']"));
 		
+		loginButton.click();
+		Thread.sleep(5000);
+		
+		assertTrue(driver.getPageSource().contains("Inbox"));
+		System.out.println("Login successfully");
+		Thread.sleep(5000);
+		driver.quit();
 	}
 	
+	@Before
+	public void setUp(){
+		System.setProperty("webdriver.gecko.driver", "D:\\Download\\geckodriver.exe");
+		  driver = new FirefoxDriver();
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		driver.quit();
+	}
 	
 }
