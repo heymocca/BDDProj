3a0c49aa3d8a9dba4148b677ad0701110be01e42
Nikki <moccababy777@gmail.com>
2016-10-04 20:49:39 +1300

    TestCucumber

--- src/cucmberTest/TestCucumber.java
+++ src/cucmberTest/TestCucumber.java
@@ -1,5 +1,20 @@
 package cucmberTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class TestCucumber {
 
+	public static WebDriver driver;
+	public static String baseUrl = "https://mail.126.com";
+	
+	@Test
+	public void testLoginAndLogout() throws Exception {
+		driver.get(baseUrl);
+		driver.get("https://mail.google.com");
+		WebElement userName = 
+	}
+	
+	
 }
